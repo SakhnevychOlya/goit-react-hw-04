0f2d525a8afe950441097f78ff8301dfaa37e571
Sakhnevych Olga <sakhnevych.olga@gmail.com>
2025-01-12 22:11:46 +0100

    color fix

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -6,7 +6,7 @@ function Loader() {
       <InfinitySpin
         visible={true}
         width="150"
-        color="#4fa94d"
+        color="rgb(36, 63, 214);"
         ariaLabel="infinity-spin-loading"
       />
     </div>
